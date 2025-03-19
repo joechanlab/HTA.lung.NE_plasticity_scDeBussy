75c828edc08cc50c93b7ef84c35b8cbf5825171f
Mamie Wang <wangm10@mskcc.org>
2025-03-19 16:02:01 -0400

    Added plots to investigate soft-DTW deeper

--- figures/test_align_genes.py
+++ figures/test_align_genes.py
@@ -4,7 +4,7 @@ import time
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from tslearn.barycenters import softdtw_barycenter
+from tslearn.barycenters import softdtw_barycenter, dtw_barycenter_averaging
 os.chdir("/data1/chanj3/HTA.lung.NE_plasticity.120122/fresh.SCLC_NSCLC.062124/results_all_genes/preprocessing/NSCLC_SCLC-A/")
 
 # get some example data
@@ -18,6 +18,13 @@ df_genes = df_genes.groupby("subject").apply(lambda x: x.sample(n=500, random_st
 df_genes = df_genes.sort_values(by=["subject", "score"], ascending=[True, True])
 subject_arrays = np.array([df_genes[df_genes['subject'] == subject].sort_values('score').iloc[:, 2:].values.tolist()
                  for subject in df_genes['subject'].unique()], dtype=object)
+# Original expression plot
+fig, ax = plt.subplots(1, 3, figsize=(10, 5))
+for i in range(3):
+    for j, subject in enumerate(df_genes['subject'].unique()):
+        ax[i].scatter(df_genes.loc[df_genes['subject'] == subject,:].iloc[:,1], df_genes.loc[df_genes['subject'] == subject,:].iloc[:,2+i], s=0.1, label=subject)
+    ax[i].legend()
+    ax[i].set_title(hvg_genes[i])
 
 # Soft-DTW on all data
 start_time = time.time()
@@ -31,7 +38,7 @@ for i in range(barycenter.shape[1]):
 ax.legend()
 plt.show()
 
-
+#---------------------------------------------------
 # Interpolate the expression (https://github.com/lindsaysmoore/cellAlign/blob/master/R/interWeights.R)
 # Define the inter_weights function
 def inter_weights(exp_data, traj_cond, win_sz, num_pts):
@@ -76,14 +83,62 @@ for subject in df_genes['subject'].unique():
 
 subject_arrays = np.array(subject_arrays, dtype=object)
 
+# Interpolated expression plot
+fig, ax = plt.subplots(1, 3, figsize=(10, 5))
+for i in range(3):
+    for j in range(subject_arrays.shape[0]):
+        ax[i].plot(traj_val_new_pts, subject_arrays[j][:,i], label=df_genes['subject'].unique()[j])
+    ax[i].legend()
+    ax[i].set_title(hvg_genes[i])
+
+# DBA on all data
+start_time = time.time()
+barycenter = dtw_barycenter_averaging(subject_arrays, max_iter=100, tol=1e-3)
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# plot the DBA barycenter
+fig, ax = plt.subplots(figsize=(10, 5))
+for i in range(barycenter.shape[1]):
+    ax.plot(barycenter[:,i], label=hvg_genes[i])
+ax.legend()
+plt.show()
+
 # Soft-DTW on all data
 start_time = time.time()
 barycenter = softdtw_barycenter(subject_arrays, gamma=1, weights=None, max_iter=100, tol=1e-3, init=None)
 print("--- %s seconds ---" % (time.time() - start_time))
 
-# plot the barycenter
+# plot the Soft-DTW barycenter
 fig, ax = plt.subplots(figsize=(10, 5))
 for i in range(barycenter.shape[1]):
     ax.plot(barycenter[:,i], label=hvg_genes[i])
 ax.legend()
+plt.show()
+
+# The effect of gamma on the Soft-DTW barycenter
+gammas = [0.1, 1, 10]
+barycenters = []
+for gamma in gammas:
+    start_time = time.time()
+    barycenter = softdtw_barycenter(subject_arrays, gamma=gamma, weights=None, max_iter=100, tol=1e-3, init=None)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    barycenters.append(barycenter)
+
+# plot the barycenter
+fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+for i in range(3):
+    for j, barycenter in enumerate(barycenters):
+        ax[i].plot(barycenter[:,i], label=gammas[j])
+    ax[i].legend()
+    ax[i].set_title(hvg_genes[i])
+plt.show()
+
+# Plot the sample and barycenter together
+fig, ax = plt.subplots(1, 3, figsize=(15, 5))
+for i in range(3):
+    for j in range(subject_arrays.shape[0]):
+        ax[i].plot(subject_arrays[j][:,i], color='gray', alpha=0.5)
+    ax[i].plot(barycenters[2][:,i], label="Barycenter", color='red')
+    ax[i].legend()
+    ax[i].set_title(hvg_genes[i])
 plt.show()
